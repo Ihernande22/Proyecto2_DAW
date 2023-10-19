9f83ed3e33389e6d0356d1359d3b4848b92c4e89
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-10-19 19:24:11 +0200

    Add files via upload

--- database/statrivals_create.sql
+++ database/statrivals_create.sql
@@ -1,1 +1,21 @@
+create database statrivals;
+use statrivals;
 
+create table Usuario (
+	ID_Usuario INT auto_increment PRIMARY KEY,
+    Nombre_Usuario varchar(255) NOT NULL,
+    Correo_Electronico varchar(255) NOT NULL,
+    Contraseña varchar(255) NOT NULL
+);
+
+create table Logro (
+	ID_Logro INT auto_increment PRIMARY KEY,
+    Descripcion text,
+    Imagen varchar(255) NOT NULL
+);
+
+create table Logro_Usuario (
+	ID_Usuario INT NOT NULL,
+    ID_Logro INT NOT NULL,
+    Porcentaje_Completo decimal(5,2) NOT NULL
+);
