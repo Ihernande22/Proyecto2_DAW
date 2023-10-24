b62aec156e22bcf02e40e34e7564e2de5a1cf078
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-10-24 16:41:53 +0200

    Add files via upload

--- database/statrivals_create.sql
+++ database/statrivals_create.sql
@@ -17,5 +17,48 @@ create table Logro (
 create table Logro_Usuario (
 	ID_Usuario INT NOT NULL,
     ID_Logro INT NOT NULL,
-    Porcentaje_Completo decimal(5,2) NOT NULL
+    Porcentaje_Completo decimal(5,2) NOT NULL,
+    foreign key (ID_Usuario) references Usuario(ID_Usuario),
+    foreign key (ID_Logro) references Logro(ID_Logro)
+);
+
+create table Liga (
+	ID_Liga INT auto_increment PRIMARY KEY,
+	Nombre_Liga varchar(255) NOT NULL,
+	Imagen varchar(255) NOT NULL
+);
+
+create table Jugador (
+	ID_Jugador INT auto_increment PRIMARY KEY,
+    ID_LIGA INT NOT NULL,
+    Nombre_Jugador varchar(255) NOT NULL.
+    Goles INT NOT NULL,
+    Asistencias INT NOT NULL,
+    Valor INT NOT NULL,
+    Partidos_Jugados INT NOT NULL,
+    Popularidad varchar(50) NOT NULL,
+    Imagen varchar(255) NOT NULL,
+    CONSTRAINT check_popularidad CHECK (popularidad IN ('media','alta')),
+    foreign key (ID_Liga) references Liga(ID_Liga)
+);
+
+create table Modo_De_Juego (
+	ID_Modo INT auto_increment PRIMARY KEY,
+    Nombre_Modo varchar(255) NOT NULL,
+    Descripcion varchar(255) NOT NULL,
+    Imagen varchar(255) NOT NULL
+);
+
+create table Registro_Partida (
+	ID_Partida INT auto_increment PRIMARY KEY,
+    ID_Usuario INT NOT NULL,
+    ID_Modo INT NOT NULL,
+    ID_Liga INT NOT NULL,
+    Dificultad varchar(50) NOT NULL,
+    Puntuacion INT NOT NULL,
+    Fecha datetime NOT NULL,
+    CONSTRAINT check_dificultad CHECK (dificultad IN ('normal','dificil')),
+    foreign key (ID_Usuario) references Usuario(ID_Usuario),
+    foreign key (ID_Modo) references Modo_De_Juego(ID_Modo),
+    foreign key (ID_Liga) references Liga(ID_Liga)
 );
