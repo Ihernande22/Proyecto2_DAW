28f8a26c019a4c8c3c885c7e7acc4610b99f6693
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-10-31 16:19:32 +0100

    Update creacion_vistas.sql

--- database/creacion_vistas.sql
+++ database/creacion_vistas.sql
@@ -1,5 +1,11 @@
+use statrivals;
+
 create view ranking_todo as 
 select u.Nombre_Usuario as Nombre_Usuario, 
 (select sum(r.Puntuacion) from Registro_Partida r
 where r.ID_Usuario = u.ID_Usuario) as Puntuacion from Usuario u;
 
+create view ranking_partida as 
+select u.Nombre_Usuario as Nombre_Usuario, 
+(select max(r.Puntuacion) from Registro_Partida r
+where r.ID_Usuario = u.ID_Usuario) as Puntuacion from Usuario u;
