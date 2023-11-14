78a540770368fd554f69d33940778802b439f393
Ihernande22 <105549591+Ihernande22@users.noreply.github.com>
2023-11-14 18:08:06 +0100

    Create crear_usuario.php

---
+++ pruebas_php/crear_usuario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crea el usuario</title>
+</head>
+<body>
+    <h1>Crea el usuario</h1>
+
+    <form>
+        <label>Correo Electronico:</label>
+        <input type ="email" name="correo"><br><br>
+
+        <label>Usuario:</label>
+        <input type="text" name="UsuarioNuevo"><br><br>
+
+        <label>Contraseña:</label>
+        <input type="text" name="ContraseñaNuevoUsuario"><br><br>
+
+        <button type="submit" name="Insertar">Insertar</button>
+
+    </form>
+    
+</body>
+</html>
