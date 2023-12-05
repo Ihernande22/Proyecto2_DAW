ccd46c426981a77dde52f417096e32606ee2c842
ivillaecija <izanvillaecija1@gmail.com>
2023-12-05 18:09:13 +0100

    Formato correo comprobado

--- statrivals-app/procesarRegistro.php
+++ statrivals-app/procesarRegistro.php
@@ -1,5 +1,48 @@
 <?php
     session_start();
+    include "VerifyEmail.php";
+
+    function validarCorreoElectronico($correo) {
+        // Expresión regular para validar el formato del correo electrónico
+        $patron = '/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/';
+    
+        // Verificar si el correo electrónico coincide con el patrón
+        if (preg_match($patron, $correo)) {
+            return -1; // El correo electrónico es válido
+        } else {
+            // El correo electrónico no es válido, proporcionar mensajes de error específicos
+    
+            // Verificar si el campo contiene un símbolo "@"
+            if (strpos($correo, '@') === false) {
+                return "El correo electrónico debe contener un símbolo '@'.";
+            }
+    
+            // Verificar si hay al menos un carácter antes y después del "@"
+            $partes = explode('@', $correo);
+            if (count($partes) !== 2 || empty($partes[0]) || empty($partes[1])) {
+                return "Debe haber al menos un carácter antes y después del símbolo '@'.";
+            }
+    
+            // Verificar si hay un punto (.) después del "@" y al menos un carácter después del punto
+            if (strpos($partes[1], '.') === false || empty(substr(strstr($partes[1], '.'), 1))) {
+                return "Después del símbolo '@' debe haber un punto (.) y al menos un carácter después del punto.";
+            }
+    
+            // Verificar si hay espacios en blanco al principio o al final del correo electrónico
+            if (trim($correo) !== $correo) {
+                return "El correo electrónico no debe contener espacios en blanco al principio o al final.";
+            }
+    
+            // Verificar si el correo electrónico contiene caracteres especiales
+            if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+                return "El correo electrónico contiene caracteres no permitidos.";
+            }
+    
+            // Devolver -1 si no hay errores
+            return -1;
+        }
+    }
+    
     //Comprueba si se ha enviado el formulario
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['registrar'])) {
@@ -7,15 +50,25 @@
             $correo = $_POST['correoElectronico'];
             $nUsuario = $_POST['nombreUsuario'];
             $password = $_POST['password'];
-
-            $validacionCorreo = true;
-            $validacionUsuario = true;
-            $validacionPassword = true;
             
+
             //Validacion correo
-            if (empty($correo)) {
-                $validacionCorreo = false;
-                $_SESSION['errorCorreoElectronicoRegistro'] = "El correo eléctronico no puede estar vacio.";
+            $validacionCorreo = validarCorreoElectronico($correo);
+            if ($validacionCorreo == -1) {
+                //Verificacion de que el correo exista(falta)
+                //$correoExiste = verificarEmailExistente($correo);
+                if ($correoExiste == -1) {
+                    //Verificación de que el correo no este repetido en la BBDD
+                    $_SESSION['verificacion'] = "ta ok";
+                }
+                else {
+                    $_SESSION['mensajeError'] = $correoExiste;
+                    header("location:registrar.php");
+                }
+            } 
+            else {
+                //No ha pasado la verificació, devuelve el mensaje de error
+                $_SESSION['mensajeError'] = $validacionCorreo;
                 header("location:registrar.php");
             }
         }
--- statrivals-app/registrar.php
+++ statrivals-app/registrar.php
@@ -12,47 +12,50 @@
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-    <!-- Logo -->
-    <a href="index.html" id="logo"><img src="img/logo.png" alt="image"></a>
+    <?php 
+        if ($_SESSION['logeado']) {
+            echo "<p class='logeadoYa'>Esta funcionalidad esta desactivada mientras la sesión esta iniciada</p>";
+        }
+        else {
+    ?>
+        <!-- Logo -->
+        <a href="index.html" id="logo"><img src="img/logo.png" alt="image"></a>
 
-    <!-- FORMULARIO LOGIN -->
-    <div id="ContenedorFormLogin">
-        <?php
-            if (isset($_SESSION['errorCorreoElectronicoRegistro'])) {
-                echo "<div class='contError'>";
-                echo "<p class='errorFormLogin'>".$_SESSION['errorCorreoElectronicoRegistro']."</p>";
-                echo "</div>";
-                $_SESSION['errorCorreoElectronicoRegistro'] = null;
-            }
-            else if (isset($_SESSION['errorNombreUsuarioRegistro'])) {
-                echo "<div class='contError'>";
-                echo "<p class='errorFormLogin'>".$_SESSION['errorNombreUsuarioRegistro']."</p>";
-                echo "</div>";
-            }
-            else if (isset($_SESSION['errorPasswordRegistro'])) {
-                echo "<div class='contError'>";
-                echo "<p class='errorFormLogin'>".$_SESSION['errorPasswordRegistro']."</p>";
-                echo "</div>";
-            }
-        ?>
-        <h2>Registrarse</h2>
-        <form action="procesarRegistro.php" method="post">
-            <input type="text" name="correoElectronico" placeholder="Correo electrónico">
-            <input type="text" name="nombreUsuario" placeholder="Nombre de usuario">
-            <input type="password" name="password" placeholder="Contraseña">
-            <button type="submit" name="registrar">Registrarse</button>
-            <hr>
-            <div id="OtrasOpciones">
-                <div id="sinUsuario">
-                    Ya tienes un usuario creado?&nbsp;<a href="login.html">Iniciar sesión</a>
+        <!-- FORMULARIO LOGIN -->
+        <div id="ContenedorFormLogin">
+            <?php
+                if (isset($_SESSION['mensajeError'])) {
+                    echo "<div class='contError'>";
+                    echo "<p class='errorFormLogin'>".$_SESSION['mensajeError']."</p>";
+                    echo "</div>";
+                    $_SESSION['mensajeError'] = null;
+                }
+                else if (isset($_SESSION['verificacion'])) {
+                    echo $_SESSION['verificacion'];
+                    $_SESSION['verificacion'] = null;
+                }
+            ?>
+            <h2>Registrarse</h2>
+            <form action="procesarRegistro.php" method="post">
+                <input type="text" name="correoElectronico" placeholder="Correo electrónico">
+                <input type="text" name="nombreUsuario" placeholder="Nombre de usuario">
+                <input type="password" name="password" placeholder="Contraseña">
+                <button type="submit" name="registrar">Registrarse</button>
+                <hr>
+                <div id="OtrasOpciones">
+                    <div id="sinUsuario">
+                        Ya tienes un usuario creado?&nbsp;<a href="login.html">Iniciar sesión</a>
+                    </div>
+                    <div id="JugarInvitado">
+                        <a href="index.html">Jugar como invitado</a>
+                    </div>
                 </div>
-                <div id="JugarInvitado">
-                    <a href="index.html">Jugar como invitado</a>
-                </div>
-            </div>
-        </form>
-    </div>
+            </form>
+        </div>
 
+    <?php
+        }
+    ?>
     <footer>
         <p>© 2023 Izan Villaécija y Iker Hernández. Todos los derechos reservados.</p>
         <a href="">Política de privacidad</a>
